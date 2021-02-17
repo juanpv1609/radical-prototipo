2cad42e8585365c9ba7f5933c6b6ec67b48dfefb
juanpv1609 <juanpv1609@gmail.com>
2021-02-17 15:51:44 -0500

    put file

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -6,7 +6,6 @@ use Carbon\Carbon;
 use App\Models\Tarea;
 use App\Models\Tareas;
 use App\Models\Contrato;
-use Illuminate\Http\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -114,7 +113,7 @@ class TareaController extends Controller
 
         $fileName = time().'-'.$request->file->getClientOriginalName();
         //$request->file->move(public_path('upload'), $fileName);
-        \Storage::disk('local')->put($fileName, \File::get($file));
+        Storage::disk('local')->put($fileName, $file);
 
 
         return response()->json(['archivo'=>$fileName]);
