84dc7846a53840c028f5cac05771035b5575bc2c
Juan Perugachi <juanpv1609@gmail.com>
2022-10-27 10:42:39 -0500

    add user notification

--- app/Console/Commands/SendAlertEndContract.php
+++ app/Console/Commands/SendAlertEndContract.php
@@ -51,7 +51,7 @@ class SendAlertEndContract extends Command
         $destinatarios = ['paul.canchignia@gruporadical.com','fabian.ortega@gruporadical.com',
                             'xavier.montoya@gruporadical.com','norma.veloz@gruporadical.com',
                             'jm.gomez@gruporadical.com','cristina.jimenez@gruporadical.com',
-                            'nelson.morales@gruporadical.com'];
+                            'nelson.morales@gruporadical.com','berioska.torres@gruporadical.com'];
         $email_cliente="";
 
              $alerta_fechas = Contrato::with('cliente')
